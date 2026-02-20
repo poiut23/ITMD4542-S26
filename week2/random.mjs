610a6c65017e145e50e9341f26444572cdd82110
Dontai Cantrell Jr <dcantrell@hawk.illinoistech.edu>
2026-02-19 19:25:59 -0600

    Update package.json and random.mjs with additional comments and structure

--- week2/random.mjs
+++ week2/random.mjs
@@ -1,3 +1,7 @@
+// below is two ways to import the node:crypto module,
+// when using node:crypto, it means importing the randomInt and randomUUID functions from the crypto module in Node.js
+// the first way is dynamic import, the second way is static import
+
 // const { randomInt, randomUUID } = await import("node:crypto");
 import { randomInt, randomUUID } from "node:crypto";
 
